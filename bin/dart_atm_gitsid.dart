cede2f43ac43670dbb0added4674b524a17853cc
joo <arihasyim.zii@gmail.com>
2021-09-03 00:17:09 +0700

    Add menu

--- bin/dart_atm_gitsid.dart
+++ bin/dart_atm_gitsid.dart
@@ -3,40 +3,54 @@ import 'dart:io';
 // akun
 var accounts = [
   {
-  'number': '01234',
+  'id': '1',
   'name': 'hasyim',
+  'pin': 'user0',
   'balance': 100000
   },
   {
-  'number': '12340',
+  'id': '2',
   'name': 'andi',
+  'pin': '123456',
   'balance': 320000
   },
   {
-  'number': '23401',
+  'id': '3',
   'name': 'aulia',
+  'pin': '123456',
   'balance': 150000
   },
   {
-  'number': '34012',
+  'id': '4',
   'name': 'rudi',
+  'pin': '123456',
   'balance': 280000
   },
   {
-  'number': '40123',
+  'id': '5',
   'name': 'putri',
+  'pin': '123456',
   'balance': 125000
   }
 ];
 
 void clear() {
-  if(Platform.isWindows) {
-    print(Process.runSync("cls", [], runInShell: true).stdout); 
-  } else {
-    print(Process.runSync("clear", [], runInShell: true).stdout);
-  }
+  print('\x1B[2J\x1B[0;0H');
+}
+
+void menu() {
+  clear();
+  print('[1] Lihat Saldo');
+  print('[2] Transfer Saldo');
+  print('[3] Tarik Tunai');
+  print('[4] Setor Tunai');
+  print('[0] Keluar');
+
+  print('Masukkan Digit Angka');
+  final menu = stdin.readLineSync();
 }
 
 void main() {
-  
+  clear();
+  menu();
 }
